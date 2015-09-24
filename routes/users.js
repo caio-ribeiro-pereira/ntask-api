a90376968860fd4f50cdaebd9c4533f4da56e149
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2015-09-24 11:56:03 -0300

    inclusao de autenticacao no routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,29 @@
 module.exports = (app) => {
   var Users = app.db.models.Users;
 
-  app.route("/users")
-    .post((req, res) => {
-      Users.create(req.body)
-        .then((result) => {
-          res.json(result);
-        })
+  app.route("/user")
+    .all(app.auth.authenticate())
+    .get((req, res) => {
+      Users.findById(req.user.id)
+        .then((result) => { res.json(result) })
         .catch((error) => {
-          res.status(412)
-            .json({msg: error.message});
+          res.status(412).json({msg: error.message});
+        });
+    })
+    .delete((req, res) => {
+      Users.destroy({where: {id: req.user.id} })
+        .then((result) => { res.sendStatus(204) })
+        .catch((error) => {
+          res.status(412).json({msg: error.message});
         });
     });
+
+  app.post("/users", (req, res) => {
+    Users.create(req.body)
+      .then((result) => { res.json(result) })
+      .catch((error) => {
+        res.status(412).json({msg: error.message});
+      });
+  });
 
 };
