e2cb3945ed1247baae7c92392048c94cf3c3c053
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2015-09-30 14:42:31 -0300

    inclusao de mais testes

--- libs/boot.js
+++ libs/boot.js
@@ -1,7 +1,9 @@
 module.exports = (app) => {
   app.db.sequelize.sync({force: true}).done(() => {
     app.listen(app.get("port"), () => {
-      console.log(`NTask API - porta ${app.get("port")}`);
+      if (process.env.NODE_ENV !== "test") {
+        console.log(`NTask API - porta ${app.get("port")}`);
+      }
     });
   });
 };
--- test/routes/token.js
+++ test/routes/token.js
@@ -1,19 +1,64 @@
 describe("Routes: Token", () => {
+  let Users = app.db.models.Users;
   describe("POST /token", () => {
-    it("returns authenticated user token", (done) => {
-      // Código do teste...
+    beforeEach((done) => {
+      Users.destroy({where: {}}).then(() => {
+        Users.create({
+          name: "John",
+          email: "john@mail.net",
+          password: "12345"
+        }).then((result) => {
+          done();
+        });
+      });
+    });
+    describe("status 200", () => {
+      it("returns authenticated user token", (done) => {
+        request.post("/token")
+          .send({
+            email: "john@mail.net",
+            password: "12345"
+          })
+          .expect(200)
+          .end((err, res) => {
+            expect(res.body).to.include.keys("token");
+            done(err);
+        });
+      });
     });
 
-    it("throws status 401 when password is incorrect", (done) => {
-      // Código do teste...
-    });
+    describe("status 401", () => {
+      it("throws error when password is incorrect", (done) => {
+        request.post("/token")
+          .send({
+            email: "john@mail.net",
+            password: "SENHA_ERRADA"
+          })
+          .expect(401)
+          .end((err, res) => {
+            done(err);
+        });
+      });
 
-    it("throws status 401 when email not exist", (done) => {
-      // Código do teste...
-    });
+      it("throws error when email not exist", (done) => {
+        request.post("/token")
+          .send({
+            email: "EMAIL_ERRADO",
+            password: "SENHA_ERRADA"
+          })
+          .expect(401)
+          .end((err, res) => {
+            done(err);
+        });
+      });
 
-    it("throws status 401 when email and password are blank", (done) => {
-      // Código do teste...
+      it("throws error when email and password are blank", (done) => {
+        request.post("/token")
+          .expect(401)
+          .end((err, res) => {
+            done(err);
+        });
+      });
     });
   });
 });
