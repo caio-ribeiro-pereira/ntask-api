f2474ba44fe914ff6480d31a3b2fe624d357baef
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2015-10-03 18:48:16 -0300

    added logs

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,11 +1,30 @@
 import bodyParser from "body-parser";
 import express from "express";
+import morgan from "morgan";
+import FSRotator from "file-stream-rotator";
+import fs from "fs";
 import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
 
 module.exports = (app) => {
+  fs.existsSync("logs") || fs.mkdirSync("logs");
   app.set("port", 3000);
   app.set("json spaces", 4);
-  app.use(cors());
+  app.use(morgan("common", {
+    stream: FSRotator.getStream({
+      filename: "logs/access-%DATE%.log",
+      frequency: "daily",
+      verbose: false
+    })
+  }));
+  app.use(helmet());
+  app.use(cors({
+    origin: ["localhost:3001"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+  }));
+  app.use(compression());
   app.use(bodyParser.json());
   app.use(app.auth.initialize());
   app.use((req, res, next) => {
@@ -13,5 +32,4 @@ module.exports = (app) => {
     next();
   });
   app.use(express.static("public"));
-
 };
