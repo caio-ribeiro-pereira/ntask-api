f10da654603b4ef88cf6aace56b17a0e7d7dc034
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2016-10-06 14:09:15 -0300

    Update middlewares.js
    Replacing the port 3000 by process.env.PORT || 3000

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -7,7 +7,7 @@ import helmet from "helmet";
 import logger from "./logger.js";
 
 module.exports = app => {
-  app.set("port", 3000);
+  app.set("port", process.env.PORT || 3000);
   app.set("json spaces", 4);
   app.use(morgan("common", {
     stream: {
